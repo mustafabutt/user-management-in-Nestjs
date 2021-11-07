1e99f8221b5522b93da14fa0e5a76d886854c3bb
Mustafa <mustafabutt69@gmail.com>
2021-11-08 04:27:25 +0500

    Update app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,14 @@
 import { Controller, Get, Request, Post, UseGuards } from '@nestjs/common';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 import { AuthService } from './auth/auth.service';
+import { globalConstants } from "./constant";
 
 @Controller()
 export class AppController {
   constructor(private authService: AuthService) {}
 
   @UseGuards(LocalAuthGuard)
-  @Post('auth/login')
+  @Post(globalConstants.LOGIN)
   async login(@Request() req) {
     return this.authService.login(req.user);
   }

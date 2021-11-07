d64f8cb48233d9ab725c967efdb2aab09f1508f2
Mustafa <mustafabutt69@gmail.com>
2021-11-08 04:22:01 +0500

    Delete dist directory

--- dist/schemas/users.schema.d.ts
+++
@@ -1,7 +0,0 @@
-import { Document } from 'mongoose';
-export declare type UserDocument = User & Document;
-export declare class User {
-    username: string;
-    password: string;
-}
-export declare const UserSchema: import("mongoose").Schema<Document<User, any, any>, import("mongoose").Model<Document<User, any, any>, any, any, any>, {}>;
